f7d491ab9303f15fc151ff5a9059025f71ae191a
Adrian Short <adrian@adrianshort.org>
2018-09-19 00:40:52 +0100

    Bugfix: must be an Integer not a String

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,7 @@ auths = UKPlanningScraper::Authority.tagged('london')
 auths.each_with_index do |auth, i|
   begin
     puts "#{i + 1} of #{auths.size}: Scraping #{auth.name}"
-    apps = auth.scrape({ decided_days: ENV['MORPH_DAYS'] })
+    apps = auth.scrape({ decided_days: ENV['MORPH_DAYS'].to_i })
     ScraperWiki.save_sqlite([:authority_name, :council_reference], apps)
     puts "#{auth.name}: #{apps.size} application(s) saved."
   rescue UKPlanningScraper::SystemNotSupportedError => e
